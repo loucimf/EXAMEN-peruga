b6d452e916865b7fc63de145843748a95a0d4a89
loucimf <145812460+loucimf@users.noreply.github.com>
2025-04-29 11:29:32 -0300

    Se rompio unity

--- Assets/PersonalTrainerScript.cs
+++ Assets/PersonalTrainerScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PersonalTrainerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum Entrenamientos
     {
-        
+        Cardio = 1000,
+        Fuerza = 1500,
+        Recreativo = 2500,
     }
 
-    // Update is called once per frame
-    void Update()
+    public int horasReservadas;
+    [field: SerializeField] private Entrenamientos eChosen;
+
+    bool ValidateData()
     {
-        
+        if (horasReservadas > 6 || horasReservadas < 0)
+        {
+            print("El numero de horas elegidas es invalido, tienen que ser menor a 6 o mayor a 0");
+            return false;
+        }
+
+        if (eChosen != Entrenamientos.Cardio &&
+            eChosen != Entrenamientos.Fuerza &&
+            eChosen != Entrenamientos.Recreativo)
+        {
+            print("Porfavor elegir un entrenamiento valido.");
+            return false;
+        }
+        return true;
+    }
+
+    private void Start()
+    {
+        if (!ValidateData())
+        {
+            return;
+        }
     }
 }
