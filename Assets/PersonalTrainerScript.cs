0b2da9c2c9caf1b1e26dd63d8ae9d7305f1c8bdb
loucimf <145812460+loucimf@users.noreply.github.com>
2025-04-29 11:47:58 -0300

    Actividad completa

--- Assets/PersonalTrainerScript.cs
+++ Assets/PersonalTrainerScript.cs
@@ -11,14 +11,28 @@ public class PersonalTrainerScript : MonoBehaviour
         Recreativo = 2500,
     }
 
+
+
+    #region Variables
     public int horasReservadas;
     [field: SerializeField] private Entrenamientos eChosen;
 
+    private int _totalPlataDiscount;
+    private int _totalPlata;
+
+    private int _plataPorHoraCardio = (int)Entrenamientos.Cardio;
+    private int _plataPorHoraRecreativo = (int)Entrenamientos.Recreativo;
+    private int _plataPorHoraFuerza = (int)Entrenamientos.Fuerza;
+    private int _plataPorHora;
+    #endregion
+
+
+
     bool ValidateData()
     {
-        if (horasReservadas > 6 || horasReservadas < 0)
+        if (horasReservadas > 6 || horasReservadas < 1)
         {
-            print("El numero de horas elegidas es invalido, tienen que ser menor a 6 o mayor a 0");
+            print("El numero de horas elegidas es invalido, tienen que ser menor a 6 o igual/mayor a 1");
             return false;
         }
 
@@ -32,11 +46,49 @@ public class PersonalTrainerScript : MonoBehaviour
         return true;
     }
 
+    int ApplyDiscount(int value)
+    {
+        print("Se aplico un descuento del 10% por reservar 6 horas!");
+        int result = value * 10 / 100;
+        return result;
+    }
+
+    void ProcessData()
+    {
+
+        if (eChosen == Entrenamientos.Cardio)
+        {
+            _plataPorHora = _plataPorHoraCardio;
+        }
+
+        if (eChosen == Entrenamientos.Fuerza)
+        {
+            _plataPorHora = _plataPorHoraFuerza;
+        }
+
+        if (eChosen == Entrenamientos.Recreativo)
+        {
+            _plataPorHora = _plataPorHoraRecreativo;
+        }
+
+        _totalPlata = _plataPorHora * horasReservadas;
+        
+        if (horasReservadas == 6)
+        {
+            _totalPlataDiscount = ApplyDiscount(_totalPlata);
+            print("El total de dinero de tu entrenamiento de tipo " + eChosen + " CON DESCUENTO es de: " + _totalPlataDiscount + "!");
+        }
+    }
+
     private void Start()
     {
         if (!ValidateData())
         {
             return;
         }
+
+        ProcessData();
+
+        print("El total de dinero de tu entrenamiento de tipo " + eChosen + " SIN DESCUENTO es de: " + _totalPlata + "!");
     }
 }
